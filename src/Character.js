cddc476ada65b624521e6e5f505e5af987df28ed
Brianna Kincart <briannakincart@gmail.com>
2020-07-01 19:15:40 -0400

    Final code from Apprenti3 clinic

--- src/Character.js
+++ src/Character.js
@@ -1,3 +1,30 @@
-// class Character {}
+class Character {
+  constructor(name, species, actor, alignmentType) {
+    this.name = name
+    this.species = species
+    this.actor = actor
+    this.alignmentType = alignmentType
+  }
 
-// export default Character
+  summary() {
+    return `${this.name} (${this.species})`
+  }
+
+  isHero() {
+    return this.alignmentType === 'hero'
+
+    // debugger
+    // if (this.alignmentType === 'hero') {
+    //   return true
+    // } else {
+    //   return false
+    // }
+
+    // if (this.alignmentType === 'hero') {
+    //   return true
+    // }
+    // return false
+  }
+}
+
+export default Character
